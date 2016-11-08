162a023fb34f97424395cd1d4afe0e74639cccf9
Yurij Kharin <yurij.kharin@gmail.com>
2016-11-08 20:15:11 +0400

    Gulp Post-css lesson

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 var gulp = require("gulp");
-var watch = require("gulp-watch");
+var watch = require("gulp-watch"),
+	postcss = require("gulp-postcss"),
+	autoprefixer = require("autoprefixer"),
+	cssvars = require('postcss-simple-vars'),
+	nested = require('postcss-nested');
 
 gulp.task('default', function(){
 	console.log("Hi! This is gulp task!");
 });
 
 gulp.task('html', function(){
-	console.log("Some special about HTML here...");
+	// console.log("Some special about HTML here...");
 });
 
 gulp.task('styles', function(){
-	console.log("Imagine CSS tasks here...");
+	// console.log("Imagine CSS tasks here...");
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssvars, nested, autoprefixer]))
+	.pipe(gulp.dest('./app/temp/styles/'));
 });
 
 gulp.task('watch', function(){
-	watch('.//app/index.html', function(){
+	watch('./app/index.html', function(){
 		gulp.start('html');
 	})
 
