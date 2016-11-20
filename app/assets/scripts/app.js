6cdd04012ac4415833d191d0309878244c4e0933
Yurij Kharin <yurij.kharin@gmail.com>
2016-11-21 00:11:23 +0400

    JS - 05 - Object with props and methods

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,14 +1,25 @@
 var john = {
 	name: "John Doe",
-	favouriteColor: "blue"
+	favouriteColor: "blue",
+	greet: function(){
+		console.log("Oh, hello");
+	}
+
 }
 
+john.greet();
+
+
+/*
 function person (name, favColor){
 	console.log("Hello, my name is " + name + " John Doe, and my favourite color is " + favColor + ".");
 }
 
 person( john.name, john.favouriteColor);
 person("Jane Smith", "green");
+
+*/
+
 
 /*
 console.log("Hello, my name is John Doe, and my favourite color is blue");
