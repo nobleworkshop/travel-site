1f731a8febda9c2d5a2cb70b2047d2c3d60025a1
Yurij Kharin <yurij.kharin@gmail.com>
2016-11-21 00:04:53 +0400

    JS - 02

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,5 +1,14 @@
+
+function person (name, favColor){
+	console.log("Hello, my name is " + name + " John Doe, and my favourite color is " + favColor + ".");
+}
+
+person("John Doe", "blue");
+person("Jane Smith", "green");
+
+/*
 console.log("Hello, my name is John Doe, and my favourite color is blue");
 console.log("Hello, my name is Jane Smith, and my favourite color is green");
-
+*/
 
 
