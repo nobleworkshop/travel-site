4173f9dd7890581cb66673009230cf7c7eba1d26
Yurij Kharin <yurij.kharin@gmail.com>
2016-11-21 08:52:01 +0400

    JS - 07 - Object with COnstructor

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,14 @@
-var john = {
-	name: "John Doe",
-	favouriteColor: "blue",
-	greet: function(){
-		console.log("Hello, my name is " + john.nsme + " John Doe, and my favourite color is " + john.favouriteColor + ".");
+function Person(name, favouriteColor) {
+	this.name = name;
+	this.favouriteColor = favouriteColor;
+	this.greet = function(){
+		console.log("Hello, my name is " + this.name + ", and my favourite color is " + this.favouriteColor + ".");
 	}
 }
 
+var john = new Person("John Doe", "green");
 john.greet();
+
+var jane = new Person("Jane Smith", "blue");
+jane.greet();
+
