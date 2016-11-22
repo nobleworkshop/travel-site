bff040ac4612ee0c229b76b2665be2a2fb327204
Yurij Kharin <yurij.kharin@gmail.com>
2016-11-22 11:17:37 +0400

    ES6 and Babel - end of the lesson

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,11 +1,20 @@
-var Person = require("./modules/Person");
+var $ = require('jquery');
 
-console.log(Person.exampleProperty);
-Person.exampleFunction();
-/*
+// require is the part of node.js
+// var Person = require("./modules/Person");
+
+// New ES6 import feature
+import Person from './modules/Person';
+
+class Adult extends Person {
+	payTaxes() {
+		console.log(this.name + "now owes $0 in taxes.");	
+	}
+}
+
 var john = new Person("John Doe", "green");
 john.greet();
 
-var jane = new Person("Jane Smith", "blue");
+var jane = new Adult("Jane Smith", "orange");
 jane.greet();
-*/
+jane.payTaxes();
--- app/assets/scripts/modules/Person.js
+++ app/assets/scripts/modules/Person.js
@@ -1,16 +1,29 @@
-function Person(name, favouriteColor) {
-	this.name = name;
-	this.favouriteColor = favouriteColor;
-	this.greet = function(){
-		console.log("Hello, my name is " + this.name + ", and my favourite color is " + this.favouriteColor + ".");
+// ES5
+// function Person(name, favouriteColor) {
+// 	this.name = name;
+// 	this.favouriteColor = favouriteColor;
+// 	this.greet = function(){
+// 	}
+// }
+
+
+// ES6
+class Person {
+	constructor(fullName, favColor){
+		this.name = fullName;
+		this.favouriteColor = favColor;
+	}
+
+	greet() {
+		console.log("Hi, my name is " + this.name + " and my fav color is " + this.favouriteColor + ".");
 	}
 }
 
+// node.js way of export
 // module.exports = Person;
 
-console.log("Hello from Person.js");
+// New ES6 way of export
+export default Person;
 
-exports.exampleProperty = "Super magical example value";
-exports.exampleFunction = function(){
-	alert("This is an example");
-}
+
+
