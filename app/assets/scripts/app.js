524234a1aa71ecb91594c2308967e960796bf938
Yurij Kharin <yurij.kharin@gmail.com>
2016-11-23 15:25:15 +0400

    Completed lazyloading images

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,9 +1,9 @@
-
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
+
 
 var mobileMenu = new MobileMenu();
 // var revealOnScroll = new RevealOnScroll();
