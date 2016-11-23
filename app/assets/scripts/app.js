d1298e6cf252f354d64c444549408aeac38a7797
Yurij Kharin <yurij.kharin@gmail.com>
2016-11-23 15:01:32 +0400

    Modal ready

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,6 +3,7 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 var mobileMenu = new MobileMenu();
 // var revealOnScroll = new RevealOnScroll();
@@ -10,6 +11,7 @@ var mobileMenu = new MobileMenu();
 new RevealOnScroll( $(".feature-item"), "85%");
 new RevealOnScroll( $(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
 
 
 /*
